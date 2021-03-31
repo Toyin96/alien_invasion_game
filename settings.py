78705fe625677d6b3cf4d024acec616b7ca0723f
Toyin96 <onagoruwam@gmail.com>
2021-04-01 00:52:42 +0100

    update basic functionality

--- settings.py
+++ settings.py
@@ -8,6 +8,6 @@ class Settings:
         """Initializes the game settings"""
         # screen settings
         self.screen_width = 1200
-        self.screen_height = 800
+        self.screen_height = 600
         self.bg_color = (230, 230, 230)
 
